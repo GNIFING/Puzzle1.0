d65f17a58c697c79a03542a5005c9f26cb3f3d2d
tharitsaengsuwan <tharit.pts@gmail.com>
2023-04-23 14:29:09 +0700

    link stamina, score, position between scene

--- Assets/Script/NextLevelDoor.cs
+++ Assets/Script/NextLevelDoor.cs
@@ -13,12 +13,24 @@ public class NextLevelDoor : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(LoadLevel());
+            float stamina = collision.gameObject.GetComponent<PlayerMovement>().getStamina();
+            int score = collision.gameObject.GetComponent<PlayerMovement>().getScore();
+            StartCoroutine(LoadLevel(stamina, score));
         }
     }
 
-    IEnumerator LoadLevel()
+    IEnumerator LoadLevel(float stamina, int score)
     {
+        if(sceneToLoad == "MainGame"){
+            PlayerPrefs.DeleteKey("inGameStamina");
+            PlayerPrefs.DeleteKey("inGameScore");
+            PlayerPrefs.DeleteKey("inGameLastLevel");
+        } else {
+            PlayerPrefs.SetFloat("inGameStamina", stamina);
+            PlayerPrefs.SetInt("inGameScore", score);
+            PlayerPrefs.SetInt("inGameLastLevel", SceneManager.GetActiveScene().buildIndex );
+        }
+
         transition.SetTrigger("FadeStart");
 
         yield return new WaitForSeconds(transitionTime);
