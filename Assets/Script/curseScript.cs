58700cbb538b5babdb8bd08d7bf338e6dd21fe46
Thanyathorn Titipatrayunyong <60185247+Herobxxm@users.noreply.github.com>
2023-05-18 14:07:47 +0700

    random position

--- Assets/Script/curseScript.cs
+++ Assets/Script/curseScript.cs
@@ -11,8 +11,14 @@ public class curseScript : MonoBehaviour
     public GameObject exitDoor;
     public float chanceToOpenExitDoor = 0.3f;
 
+    // list of pattern of doors to remove
+    List<int> numList;
+
     // list of prefab of enemies to spawn
     public List<GameObject> enemies;
+
+    // trash prefab
+    public GameObject trash;
 
     // golden trash
     public GameObject goldenTrash;
@@ -26,8 +32,50 @@ public class curseScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // random 0-99
+        int rand = Random.Range(0, 100);
+        // if random is 0-20    // except 6
+        if (rand < 10)
+        {
+            numList = new List<int> {0,2,3,4,5,7,9};
+        }
+        else if (rand < 20)
+        {
+            numList = new List<int> {0,2,3,4,5,7,9,10};
+        }
+        else if (rand < 30)
+        {
+            numList = new List<int> {0,2,3,4,5,7,10};
+        }
+        else if (rand < 40)
+        {
+            numList = new List<int> {0,3,4,7,8,9};
+        }
+        else if (rand < 60)
+        {
+            numList = new List<int> {1,2,3,7,8,9,10};
+        }
+        else if (rand < 80)
+        {
+            numList = new List<int> {0,1,3,4,9,10};
+        }
+        else if (rand < 90)
+        {
+            numList = new List<int> {0,1,2,3,4};
+        }
+        else if (rand < 94)
+        {
+            numList = new List<int> {0,1,2,4,9};
+        }
+        else if (rand < 97)
+        {
+            numList = new List<int> {0,1,2,3,4,5,7,8,9,10};
+        } 
+        else
+        {
+            numList = new List<int> {};
+        }
 
-        navMeshSurface = navMesh.GetComponent<NavMeshSurface2d>();
 
         // destroy exit doors on chance
         if (Random.Range(0.0f, 1.0f) < chanceToOpenExitDoor)
@@ -36,20 +84,17 @@ public class curseScript : MonoBehaviour
         }else{
             // destroy golden door( door 6 )
             Destroy(doors[6]);
-            doors.RemoveAt(6);
-        }
-        // random destroy doors
-        for (int i = 0; i < doors.Count; i++)
-        {
-            if (Random.Range(0.0f, 1.0f) < 0.7f)
-            {
-                // Disable NavMeshObstacle
-                // doors[i].GetComponent<NavMeshObstacle>().enabled = false;
-                Destroy(doors[i]);
-                doors.RemoveAt(i);
-            }
         }
 
+
+
+        // destroy door on list
+        foreach (int num in numList)
+        {
+            Destroy(doors[num]);
+        }
+
+        navMeshSurface = navMesh.GetComponent<NavMeshSurface2d>();
         isNavMeshBuilt = true;
 
     }
@@ -63,7 +108,6 @@ public class curseScript : MonoBehaviour
             isNavMeshBuilt = false;
         }
 
-
         // check if golden trash is null
         if (goldenTrash == null)
         {
@@ -71,6 +115,40 @@ public class curseScript : MonoBehaviour
             Destroy(exitDoor);
         }
 
-
     }
+
+    void spawnEnemy()
+    {
+        // spawn enemy
+        int rand = Random.Range(0, enemies.Count);
+        Instantiate(enemies[rand], transform.position, Quaternion.identity);
+    }
+    // void spawnTrash()
+    // {
+    //     // get random position on navmesh
+    //     Vector3 randomPosition = navMesh.GetComponent<NavMeshSurface2d>().GetRandomPoint();
+
+    //     // spawn trash
+    //     GameObject trashObj = Instantiate(trash, randomPosition, Quaternion.identity);
+
+    //     int rand = Random.Range(0, 100);
+    //     if (rand > 95)
+    //     {
+    //         // get trash child
+    //         Transform Transform = trashObj.transform.Find("Trash");
+    //         // change trash script - public score to 4
+    //         Transform.GetComponent<Trash>().score = 4;
+    //         // change color to green
+    //         Transform.GetComponent<SpriteRenderer>().color = new Color(0.0f, 1.0f, 0.0f, 1.0f);
+    //     }
+    //     else if (rand > 80)
+    //     {
+    //         // get trash child
+    //         Transform Transform = trashObj.transform.Find("Trash");
+    //         // change trash script - public score to 2
+    //         Transform.GetComponent<Trash>().score = 2;
+    //         // change color to red
+    //         Transform.GetComponent<SpriteRenderer>().color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    //     }
+    // }
 }
